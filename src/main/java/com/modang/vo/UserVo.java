9855a9f83055f79a18bcb4ba7a728de8937cb4a7
cheoho-hi <cheoho-hi@DESKTOP-0HKEG65>
2023-07-13 09:22:26 +0900

    Merge branch 'master' of https://github.com/Anthony-Yoo/Modang.git

--- src/main/java/com/modang/vo/UserVo.java
+++ src/main/java/com/modang/vo/UserVo.java
@@ -2,18 +2,6 @@ package com.modang.vo;
 
 public class UserVo {
 	
-<<<<<<< HEAD
-	private int userno;
-	private String id;
-	private String passwd;
-	private String nick;
-	private String cellphone;
-	private int average;
-	private int recommaverage;
-	private String recentlogontime;
-	private long profileimage;
-	private int keynum;
-=======
 	private int userno;					/*유저엔티티번호*/
 	private String id;					/*아이디*/
 	private String passwd;				/*비밀번호*/
@@ -26,12 +14,14 @@ public class UserVo {
 	private int keynum;					/*키넘버*/
 	private String createdate;			/*생성시간*/
 	private int keycount;				/*생성횟수*/
+	
 	public UserVo() {
 		super();
 	}
->>>>>>> branch 'master' of https://github.com/Anthony-Yoo/Modang.git
+
 	public UserVo(int userno, String id, String passwd, String nick, String cellphone, int average, int recommaverage,
-			String recentlogontime, long profileimage, int keynum) {
+			String recentlogontime, long profileimage, int keynum, String createdate, int keycount) {
+		super();
 		this.userno = userno;
 		this.id = id;
 		this.passwd = passwd;
@@ -42,75 +32,112 @@ public class UserVo {
 		this.recentlogontime = recentlogontime;
 		this.profileimage = profileimage;
 		this.keynum = keynum;
+		this.createdate = createdate;
+		this.keycount = keycount;
 	}
-	public UserVo() {
-	}
+
 	public int getUserno() {
 		return userno;
 	}
+
 	public void setUserno(int userno) {
 		this.userno = userno;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPasswd() {
 		return passwd;
 	}
+
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
 	}
+
 	public String getNick() {
 		return nick;
 	}
+
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
+
 	public String getCellphone() {
 		return cellphone;
 	}
+
 	public void setCellphone(String cellphone) {
 		this.cellphone = cellphone;
 	}
+
 	public int getAverage() {
 		return average;
 	}
+
 	public void setAverage(int average) {
 		this.average = average;
 	}
+
 	public int getRecommaverage() {
 		return recommaverage;
 	}
+
 	public void setRecommaverage(int recommaverage) {
 		this.recommaverage = recommaverage;
 	}
+
 	public String getRecentlogontime() {
 		return recentlogontime;
 	}
+
 	public void setRecentlogontime(String recentlogontime) {
 		this.recentlogontime = recentlogontime;
 	}
+
 	public long getProfileimage() {
 		return profileimage;
 	}
+
 	public void setProfileimage(long profileimage) {
 		this.profileimage = profileimage;
 	}
+
 	public int getKeynum() {
 		return keynum;
 	}
+
 	public void setKeynum(int keynum) {
 		this.keynum = keynum;
 	}
+
+	public String getCreatedate() {
+		return createdate;
+	}
+
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+
+	public int getKeycount() {
+		return keycount;
+	}
+
+	public void setKeycount(int keycount) {
+		this.keycount = keycount;
+	}
+
 	@Override
 	public String toString() {
 		return "UserVo [userno=" + userno + ", id=" + id + ", passwd=" + passwd + ", nick=" + nick + ", cellphone="
 				+ cellphone + ", average=" + average + ", recommaverage=" + recommaverage + ", recentlogontime="
-				+ recentlogontime + ", profileimage=" + profileimage + ", keynum=" + keynum + "]";
+				+ recentlogontime + ", profileimage=" + profileimage + ", keynum=" + keynum + ", createdate="
+				+ createdate + ", keycount=" + keycount + "]";
 	}
-	
 	
 }
