9ce638cefb69895b76a2a65563873d278bd5ccba
hyesun1022 <blue1708@naver.com>
2023-07-26 17:52:45 +0900

    혜선 업데이트

--- src/main/java/com/modang/service/BiliardService.java
+++ src/main/java/com/modang/service/BiliardService.java
@@ -22,7 +22,8 @@ public class BiliardService {
 	@Autowired
 	private BiliardDao biliardDao;
 	
-	/* 테이블 추가 */
+	/* 테이블현황 페이지-------------------------------------------------------------------------------- */
+	/* 테이블 현황 - 테이블 추가 */
 	public int addTable(CueTableVo cuetableVo) {
 		System.out.println("BiliardService.addTable()");
 		
@@ -30,7 +31,7 @@ public class BiliardService {
 		return count;
 	}
 	
-	/* 테이블 현황-테이블 종류 변경 */
+	/* 테이블 현황 - 테이블 종류 변경 */
 	public int tableTypeModify(CueTableVo cuetableVo) {
 		System.out.println("BiliardService.tableTypeModify()");
 		
@@ -38,15 +39,19 @@ public class BiliardService {
 		return count;
 	}
 	
+	/* 테이블 현황 - 게임정보 가져오기(미정산리스트만) */
+	public void getIncaluList() {
+		System.out.println("BiliardService.getIncaluList()");
+		
+	}
 	
-	/* 게임정보 가져오기 */
+	/* 테이블 현황 - 게임정보 가져오기 */
 	public List<TableGamesVo> getGames(int biliardNo) {
 		System.out.println("BiliardService.getGames()");
 		List<TableGamesVo> gamesList = biliardDao.selectGames(biliardNo);
 		System.out.println(gamesList);
 		
 		return gamesList;
-		
 	}
 	
 	/* 테이블 현황 - 상세정보 가져오기 */
@@ -58,7 +63,6 @@ public class BiliardService {
 		TableGamesVo gamesVo = biliardDao.selectTable(cuetableVo);
 		TariffVo tariffVo = biliardDao.selectPrice(biliardNo);
 		CueTableVo oneTable = biliardDao.selectCuetable(tableNo);
-		
 				
 		List<CueTableVo> cueTableList = biliardDao.selectList(biliardNo);
 		
@@ -72,18 +76,18 @@ public class BiliardService {
 		return tMap;
 	}
 	
-	/*테이블현황-테이블 전체리스트 가져오기*/
+	/*테이블 현황 - 테이블 전체리스트 가져오기*/
 	public List<CueTableVo> tableList(int biliardNo) {
 		System.out.println("BiliardService.tableList()");
 		
 		List<CueTableVo> cueTableList = biliardDao.selectList(biliardNo);
-		
 		System.out.println(cueTableList);
 		
 		return cueTableList;
 	}
 	
-	/* 요금테이블-요금정보 가져오기 */
+	/* 요금테이블 페이지--------------------------------------------------------------------------------- */
+	/* 요금테이블 - 요금정보 가져오기 */
 	public TariffVo getPrice(int biliardNo) {
 		System.out.println("BiliardService.getPrice()");
 		
@@ -91,12 +95,19 @@ public class BiliardService {
 		return tariffVo;
 	}
 	
-	/* 요금테이블-요금수정 */
+	/* 요금테이블 - 요금수정 */
 	public int updatePrice(TariffVo tariffVo) {
 		System.out.println("BiliardService.updatePrice()");
 		
 		int count = biliardDao.updatePrice(tariffVo);
 		return count;
+	}
+	
+	/* 관리자 설정 페이지-------------------------------------------------------------------------------- */
+	/* 관리자 설정 - 당구장 정보 불러오기 */
+	public void getbiliardInfo(ManagerVo loginManager) {
+		System.out.println("BiliardService.getbiliardInfo()");
+		
 	}
 
 }
