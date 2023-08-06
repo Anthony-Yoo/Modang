66a14c75442d0c33b74db1b2b49b05e04ea55169
Anthony-Yoo <134242160+Anthony-Yoo@users.noreply.github.com>
2023-08-06 15:47:09 +0900

    8/6 업데이트

--- src/main/java/com/modang/vo/RecordUserVo.java
+++ src/main/java/com/modang/vo/RecordUserVo.java
@@ -4,9 +4,9 @@ public class RecordUserVo {
 	
 	private int recordNo;
 	private int playNo;
-	private int userNo;
-	private int gameNo;
-	private String nick;
+	private int userNo;				//최근 3경기 함께한 유져번호
+	private int gameNo;				
+	private String nick;			//최근 3경기 함께한 유져 닉
 	private int average;
 	private int recommAverage;
 	private int currentAverage;
@@ -25,21 +25,25 @@ public class RecordUserVo {
 	private int paymoney;	
 	private int calGameTime;
 	private String totalGameTime;	
-	private double totalWinRate;
-	private int totalCountGame;	
-	private int totalCountWinGame;
-	private int totalCountNormGame;			// 누적 본인다마 전체게임수	
-	private int totalCountWinNormGame;			// 누적 본인다마 승리게임수	
-	private int totalCountLoseNormGame;		// 누적 본인다마 패배게임수
-	private double totalNormRate; 			// 본인 다마 안물린비율
-	private int totalNormRatePercent;		// 누적 본인다마 안물린승률 100분위
+	private double totalWinRate;            // 전체 누적 (본인다마+정상게임) 승률
+	private double totalCommRate;			// 누적 안물린 비율(new)(본인다마 +정상게임) 
+	private int totalCountGame;				// 전체 누적 (본인다마+정상게임) 게임수
+	private int totalCountWinGame;			// 전체 누적 (본인다마+정상게임) 승리게임수	
+	private int totalCountLoseGame;			// 전체 누적 (본인다마+정상게임) 패배게임수	
+	private int totalCountNormGame;			
+	private int totalCountWinNormGame;		
+	private int totalCountLoseNormGame;	
+	private double totalNormRate; 		
+	private int totalNormRatePercent;	
 	private int recommStatus;				// 권장다마 상태
+	private String totalPlayTime;			// 전체 누적(본인다마 관계없음 / 나가리게임포함) 플레이시간
 	public RecordUserVo(int recordNo, int playNo, int userNo, int gameNo, String nick, int average, int recommAverage,
 			int currentAverage, int activeAverage, int hitMarker, String profileImage, String biliardAddress,
 			String biliardName, int tableType, int gameType, int memberNum, String gameDate, String gameTime,
 			String playTime, int record, int paymoney, int calGameTime, String totalGameTime, double totalWinRate,
-			int totalCountGame, int totalCountWinGame, int totalCountNormGame, int totalCountWinNormGame,
-			int totalCountLoseNormGame, double totalNormRate, int totalNormRatePercent, int recommStatus) {
+			double totalCommRate, int totalCountGame, int totalCountWinGame, int totalCountLoseGame,
+			int totalCountNormGame, int totalCountWinNormGame, int totalCountLoseNormGame, double totalNormRate,
+			int totalNormRatePercent, int recommStatus, String totalPlayTime) {
 		this.recordNo = recordNo;
 		this.playNo = playNo;
 		this.userNo = userNo;
@@ -64,14 +68,17 @@ public class RecordUserVo {
 		this.calGameTime = calGameTime;
 		this.totalGameTime = totalGameTime;
 		this.totalWinRate = totalWinRate;
+		this.totalCommRate = totalCommRate;
 		this.totalCountGame = totalCountGame;
 		this.totalCountWinGame = totalCountWinGame;
+		this.totalCountLoseGame = totalCountLoseGame;
 		this.totalCountNormGame = totalCountNormGame;
 		this.totalCountWinNormGame = totalCountWinNormGame;
 		this.totalCountLoseNormGame = totalCountLoseNormGame;
 		this.totalNormRate = totalNormRate;
 		this.totalNormRatePercent = totalNormRatePercent;
 		this.recommStatus = recommStatus;
+		this.totalPlayTime = totalPlayTime;
 	}
 	public RecordUserVo() {
 	}
@@ -219,6 +226,12 @@ public class RecordUserVo {
 	public void setTotalWinRate(double totalWinRate) {
 		this.totalWinRate = totalWinRate;
 	}
+	public double getTotalCommRate() {
+		return totalCommRate;
+	}
+	public void setTotalCommRate(double totalCommRate) {
+		this.totalCommRate = totalCommRate;
+	}
 	public int getTotalCountGame() {
 		return totalCountGame;
 	}
@@ -230,6 +243,12 @@ public class RecordUserVo {
 	}
 	public void setTotalCountWinGame(int totalCountWinGame) {
 		this.totalCountWinGame = totalCountWinGame;
+	}
+	public int getTotalCountLoseGame() {
+		return totalCountLoseGame;
+	}
+	public void setTotalCountLoseGame(int totalCountLoseGame) {
+		this.totalCountLoseGame = totalCountLoseGame;
 	}
 	public int getTotalCountNormGame() {
 		return totalCountNormGame;
@@ -267,6 +286,12 @@ public class RecordUserVo {
 	public void setRecommStatus(int recommStatus) {
 		this.recommStatus = recommStatus;
 	}
+	public String getTotalPlayTime() {
+		return totalPlayTime;
+	}
+	public void setTotalPlayTime(String totalPlayTime) {
+		this.totalPlayTime = totalPlayTime;
+	}
 	@Override
 	public String toString() {
 		return "RecordUserVo [recordNo=" + recordNo + ", playNo=" + playNo + ", userNo=" + userNo + ", gameNo=" + gameNo
@@ -276,12 +301,13 @@ public class RecordUserVo {
 				+ tableType + ", gameType=" + gameType + ", memberNum=" + memberNum + ", gameDate=" + gameDate
 				+ ", gameTime=" + gameTime + ", playTime=" + playTime + ", record=" + record + ", paymoney=" + paymoney
 				+ ", calGameTime=" + calGameTime + ", totalGameTime=" + totalGameTime + ", totalWinRate=" + totalWinRate
-				+ ", totalCountGame=" + totalCountGame + ", totalCountWinGame=" + totalCountWinGame
-				+ ", totalCountNormGame=" + totalCountNormGame + ", totalCountWinNormGame=" + totalCountWinNormGame
-				+ ", totalCountLoseNormGame=" + totalCountLoseNormGame + ", totalNormRate=" + totalNormRate
-				+ ", totalNormRatePercent=" + totalNormRatePercent + ", recommStatus=" + recommStatus + "]";
+				+ ", totalCommRate=" + totalCommRate + ", totalCountGame=" + totalCountGame + ", totalCountWinGame="
+				+ totalCountWinGame + ", totalCountLoseGame=" + totalCountLoseGame + ", totalCountNormGame="
+				+ totalCountNormGame + ", totalCountWinNormGame=" + totalCountWinNormGame + ", totalCountLoseNormGame="
+				+ totalCountLoseNormGame + ", totalNormRate=" + totalNormRate + ", totalNormRatePercent="
+				+ totalNormRatePercent + ", recommStatus=" + recommStatus + ", totalPlayTime=" + totalPlayTime + "]";
 	}
 	
-
+	
 }
 
